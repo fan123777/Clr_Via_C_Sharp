25fbf97fffbfeb97af167c99409f047b1538155e
fan123777 <andrey.fonaskov@gmail.com>
2016-04-08 16:32:21 +0300

    chapter 9

--- CLR_VIA_C_SHARP/Program.cs
+++ CLR_VIA_C_SHARP/Program.cs
@@ -26,8 +26,9 @@ namespace CLR_VIA_C_SHARP
                 _2_Type_Design._6_MembersAndTypes.MembersAndTypes.main();
                 _2_Type_Design._7_ConstantsAndFields.ConstantsAndFields.main();
                 _2_Type_Design._8_Methods.Methods.main();
+                _2_Type_Design._9_Parameters.Parameters.main();
             }
-            _2_Type_Design._9_Parameters.Parameters.main();
+            _2_Type_Design._10_Properties.Properties.main();
         }
     }
 }

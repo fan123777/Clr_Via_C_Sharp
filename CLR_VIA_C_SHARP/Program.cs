2488a3c666b6935400cb75326271921fc23d77c1
fan123777 <andrey.fonaskov@gmail.com>
2016-04-27 15:17:06 +0300

    chapter12

--- CLR_VIA_C_SHARP/Program.cs
+++ CLR_VIA_C_SHARP/Program.cs
@@ -28,8 +28,9 @@ namespace CLR_VIA_C_SHARP
                 _2_Type_Design._8_Methods.Methods.main();
                 _2_Type_Design._9_Parameters.Parameters.main();
                 _2_Type_Design._10_Properties.Properties.main();
+                _2_Type_Design._11_Events.Events.main();
             }
-            _2_Type_Design._11_Events.Events.main();
+            _2_Type_Design._12_Generic.Generic.main();
         }
     }
 }
